7046a0adcd1a835207e32e5210bb765df3a1ebcb
cmc:w7:home <cceballosq@gmial.com>
2015-02-06 02:15:02 -0600

    90 % de generar recibo

--- recibos.php
+++ recibos.php
@@ -254,11 +254,11 @@
   		 http://stackoverflow.com/questions/10521165/bootstrap-javascript-carousel-doesnt-stop-cycling
 		
       	-->
-      <div class="modal-body" style="overflow:hidden; border:0px solid;">      	
-      	<div class="bs-example" syle="width:100%; overflow:hidden; margin:-10px">
+      <div class="modal-body" style="border:0px solid; overflow:hidden;">  <!-- overflow:hidden-->    	
+      	<div class="bs-example" syle="width:100%; overflow:hidden; margin:-10px"><!-- overflow:hidden-->
 		    <div id="myCarousel" class="carousel slide" data-interval="false" data-ride="carousel" style="width:80%; float:left; border:0px solid; margin:0px; padding:0px;">
 		        <!-- Carousel items -->
-		        <div class="carousel-inner " style="whidth:100%; padding:0px; height:1000px">
+		        <div class="carousel-inner " style="whidth:100%; padding:0px;">
 		        	
 
 		            <div class="active item" style="background-color: white; padding:0px; width:100%; ">
@@ -292,67 +292,144 @@
 		            		</div>
 	                	</div>
 
-	                	<div class="row " style="">
+	                	<div class="row " style=" ">
 	                		<div class="col-md-12">
-		            			<div class="col-md-12" >
-		            				<div class="col-md-12" style="border-width: 0px 1px 1px 1px; border-style: solid; border-color: gray;">
-		            					<div class="col-md-9" style="margin-top: 4px">
-		            						 	<input type="text" maxlength="30" value="Nombre/Razón Social del Inquilino" class="form-control superradiusin input-sm" id="ninompais" name="ninompais"  placeholder="">					   
+
+		            			<div class="col-md-9" style ="" >
+		            				<div class="col-md-12" style="border-width: 0px 0px 1px 1px; border-style: solid; border-color: gray;">
+		            					<div class="col-md-9" style="margin-top: 8px">
+		            						 	<input type="text" maxlength="30" value="Nombre/Razón Social del Inquilino" class="form-control superradiusin input-sm" id="" name=""  placeholder="">					   
 		            					</div>
 		            					<div class="col-md-10 " style="margin-left:-15px; margin-top: 4px">
 		            						<div class="col-md-6">		            							
-		            								<input type="text" maxlength="30" value="Nombre de Calle" class="form-control superradiusin input-sm" id="ninompais" name="ninompais"  placeholder="">					   		            									               							
+		            								<input type="text" maxlength="30" value="Nombre de Calle" class="form-control superradiusin input-sm" id="" name=""  placeholder="">					   		            									               							
 		            						</div>
 		            						<div class="col-md-3">
-		            							<input type="text" maxlength="30" value="Núm ext" class="col-md-pull-1 form-control superradiusin input-sm col-md-4" id="ninompais" name="ninompais"  placeholder="">					   		            							
+		            							<input type="text" maxlength="30" value="Núm ext" class="col-md-pull-1 form-control superradiusin input-sm col-md-4" id="" name=""  placeholder="">					   		            							
 		            						</div>
 		            						<div class="col-md-3">
-		            								 	<input type="text" maxlength="30" value="Núm int" class="col-md-pull-3 form-control superradiusin input-sm col-md-4" id="ninompais" name="ninompais"  placeholder="">					   		            					
+		            								 	<input type="text" maxlength="30" value="Núm int" class="col-md-pull-3 form-control superradiusin input-sm col-md-4" id="" name=""  placeholder="">					   		            					
 		            					 	</div>
 		            					</div>
 		            					<div class="col-md-8" style="margin-left:-15px; margin-top: 4px">
 		            						<div class="col-md-11" >			            							
-			            							<input type="text" maxlength="30" value="Colonia" class="form-control superradiusin input-sm" id="ninompais" name="ninompais"  placeholder="">					   		            									               							
+			            							<input type="text" maxlength="30" value="Colonia" class="form-control superradiusin input-sm" id="" name=""  placeholder="">					   		            									               							
 							           						
             								</div>		            						
 		            					</div>
 		            					
 		            					<div class="col-md-8" style="margin-left:-15px; margin-top: 4px">
 		            						<div class="col-md-5" >			            							
-			            							<input type="text" maxlength="30" value="Municipio" class="form-control superradiusin input-sm" id="ninompais" name="ninompais"  placeholder="">					   		            									               											           							
+			            							<input type="text" maxlength="30" value="Municipio" class="form-control superradiusin input-sm" id="" name=""  placeholder="">					   		            									               											           							
             								</div>	
             								<div class="col-md-5" >			            							
-			            							<input type="text" maxlength="30" value="Estado" class="form-control superradiusin input-sm" id="ninompais" name="ninompais"  placeholder="">					   		            									               											           							
+			            							<input type="text" maxlength="30" value="Estado" class="form-control superradiusin input-sm" id="" name=""  placeholder="">					   		            									               											           							
             								</div>		            						
 		            					</div>
-		            					<div class="col-md-8" style="margin-left:-15px; margin-top: 4px">
+		            					<div class="col-md-8" style="margin-left:-15px; margin-top: 4px; margin-bottom:8px;">
 		            						<div class="col-md-5" >			            							
-			            							<input type="text" maxlength="30" value="País" class="5 form-control superradiusin input-sm" id="ninompais" name="ninompais"  placeholder="">					   		            									               											           							
+			            							<input type="text" maxlength="30" value="País" class="5 form-control superradiusin input-sm" id="" name=""  placeholder="">					   		            									               											           							
             								</div>		
             								<div class="col-md-5" >			            							
-			            							<input type="text" maxlength="30" value="C.P" class="form-control superradiusin input-sm" id="ninompais" name="ninompais"  placeholder="">					   		            									               											           							
+			            							<input type="text" maxlength="30" value="C.P" class="form-control superradiusin input-sm" id="" name=""  placeholder="">					   		            									               											           							
             								</div>	            						
 		            					</div>
-		            				
+		            				</div>
+		            			</div>
+		            			<div class="col-md-3" style="border-width: 0px 1px 1px 0; border-style: solid; border-color: gray; height:181px; margin-left:-15px;" >
+			            			<div class="col-md-12 col-md-pull-3" >
+			            				<div class="col-md-5" style="">
 
 
 
+			            				</div>
+			            			</div>
+            					</div>	
+		            		</div>
+		            		
+	                	</div>
+
+	                	<div class="row " style="margin-top:20px">
+	                		<div class="col-md-12">
+		            			<div class="col-md-12" >
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px; padding-top: 10px;">Cantidad</div>
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px;">Unidad de Medida</div>
+		            				<div class="col-md-4" style="border: 1px solid gray; height:44px;">Descripción de Concepto</div>
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px;">Precio Unitario</div>
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px; padding-top: 10px;">Importe</div>
+		            			</div>
+		            		</div>
+	                	</div>
+
+	                	<div class="row " style="margin-top:0px">
+	                		<div class="col-md-12">
+		            			<div class="col-md-12" >
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px; padding-top: 10px;">1</div>
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px; padding-top: 5px;">
+		            					<input type="text" maxlength="30" value="Renta" class="form-control superradiusin input-sm" id="" name=""  placeholder="">					   		            									               											           							            								
+		            				</div>
+		            				<div class="col-md-4" style="border: 1px solid gray">
+		            					<textarea class="form-control superradiusin" rows="2" id="nicomentarios" style="resize: none; padding:0px;">Renta de Casa de Monterrey en Ciudad SatéliteNúmero </textarea>
+		            				</div>
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px; padding-top: 5px;">
+		            					<input type="text" maxlength="30" value="10,000.00" class="form-control superradiusin input-sm" id="" name=""  placeholder="">
+		            				</div>
+		            				<div class="col-md-2" style="border: 1px solid gray; height:44px; padding-top: 10px;">10,000.00</div>		            				
+		            			</div>
+		            		</div>
+	                	</div>
+
+	                	<div class="row " style="margin-top:10px">
+	                		<div class="col-md-12">
+		            			<div class="col-md-12 " >
+		            				<div class="col-md-7 col-md-pull-0" style="height:44px; padding-top: 10px;">
+		            					<button  class="col-md-5 col-md-pull-0" type="button" class="btn btn-mini superradiusin" style="color: gray; border: 3px solid #FAFAFA; border-top: none; border-left: none; background-color:white;"> <strong>Agregar Concepto</strong> </button>
 		            				</div>
 		            			</div>
 		            		</div>
 	                	</div>
+
+	                	<div class="row " style="margin-top:20px;">
+	                		<div class="col-md-12">
+		            			<div class="col-md-12" style="" >
+		            				<div class="col-md-8" style=" padding-top: 10px;  border: 1px solid gray;">
+		            					<div class="col-md-12 col-md-pull-1 text-left " style=" ">
+		            					<div  class="col-md-4"><strong>Forma de pago: </strong></div>	<div class="col-md-6 col-md-pull-1">Pago en una sola exhibición</div>
+		            					</div>
+		            					<div class="col-md-12 col-md-pull-1 text-left" style="">		            						
+		            						<div  class="col-md-4"><strong>Método de pago: </strong></div>	
+		            						<div class="col-md-6 col-md-pull-0">
+		            							<input type="text" maxlength="30" value="No identificado" class="form-control superradiusin input-sm" id="" name=""  placeholder="">		            				
+		            						</div>		            					
+		            					</div>
+		            					<div class="col-md-12 col-md-pull-1 text-left" style="padding-top: 10px;">
+		            						<div  class="col-md-5"><strong>Lugar de expedición:</strong></div>	<div class="col-md-6 col-md-pull-1">Nuevo León</div>
+		               					</div>
+		            				</div>	
+		            				<div class="col-md-4 zxc " style=" padding-top: 10px;   border: 1px solid gray; border-left: 0px; height: 93px;">
+		            					<div class="col-md-12" style="padding-top: 5px;">
+		            						<div  class="col-md-4 col-md-pull-2">Subtotal</div>	<div class="col-md-6 col-md-push-2 ">$10,000.00</div>
+		               					</div>
+		               					
+		               					<div class="col-md-12" style="padding-top: 10px; border-top:1px solid; margin-top: 20px">
+		            						<div  class="col-md-4 col-md-pull-2"><strong>Total</strong></div>	<div class="col-md-6 col-md-push-2 text-rigth"><strong>$10,000.00</strong></div>
+		               					</div>
+
+		            				</div>            				
+		            				
+		            			</div>
+		            		</div>
+	                	</div>
+
+	                	
 	                
-
-
-
-
 
 		                
 
 		            </div>
 		            <div class="item" >		 
-
-		                <input type="text">
+		            	<!-- segundo slider -->
+		                
 		            </div>
 		            <div class="item" style="background-color: white;">
 		            	<input type="text">
@@ -367,7 +444,7 @@
 		        </div>
 		    </div>
 		    <!-- Controls buttons -->
-		    <div class="control-buttons" style="color:#1db2a4; width:18%; min-height:500px; float:left; border-left: 1px solid gray; padding-top:15px;">		        		       
+		    <div class="control-buttons" style="color:#1db2a4; width:18%; min-height:700px; float:left; border-left: 1px solid gray; padding-top:15px;">		        		       
 		        <div class="row">
 		        	<div class="btn" id="uno" onClick="cambiarform(0)" class="col-md-12"> <strong>Casa Monterrey...</strong>  </div>
 		        </div>	 
